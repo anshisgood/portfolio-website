738ab92e016141ced6096a069bae28e4e7c27f07
Ansh Singh <s.ansh3007@gmail.com>
2025-11-27 03:03:34 -0600

    added basic structure to menu layout.

--- app/screens/menuscreen.tsx
+++ app/screens/menuscreen.tsx
@@ -4,6 +4,27 @@ export default function MenuScreen() {
     return (
       <div className="menu-container">
         <p>need to make..</p>
+
+        <img src={"/images/wii-button.svg"} style={{position: "absolute", width: "9vw", bottom: "1.9%", left: "4.6%"}}></img>
+        <img src={"/images/wii-SD.png"} style={{position: "absolute", width: "4vw", bottom: "1.9%", left: "18.5%"}}></img>
+        
+        <div className="menu-items-container">
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+
+          <div className="item"></div>
+          <div className="item"></div>
+          {/* <div className="item"></div>
+          <div className="item"></div>
+
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div> */}
+        </div>
+      
       </div>
     )
 }
